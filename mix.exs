02adbd88a72adfae3b6d100f6f018a5776c3f31e
Karl Eklund <localpart@gmail.com>
2019-01-15 10:39:08 +0100

    Elixir 1.8 released

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Game.MixProject do
     [
       app: :game,
       version: "0.1.0",
-      elixir: "~> 1.8-rc",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
