429a20ce60568b59a396473458c89bf02cc0867e
Karl Eklund <localpart@gmail.com>
2019-01-19 13:34:18 +0100

    Add supervisor

---
+++ lib/game/application.ex
@@ -0,0 +1,20 @@
+defmodule Game.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Game.Worker.start_link(arg)
+      # {Game.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Game.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
