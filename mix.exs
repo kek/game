1e8f021b85dfcea2d3d544208c5e863ff9c0e81d
Karl Eklund <localpart@gmail.com>
2019-02-10 00:45:41 +0100

    Add Symbelix as dependency

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Game.MixProject do
   defp deps do
     [
       {:observer_cli, "~> 1.4"},
-      {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
+      {:symbelix, github: "kek/symbelix"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
