579cab3171c16b385bd1a14d21690ceb0827b305
Karl Eklund <localpart@gmail.com>
2019-02-10 17:11:20 +0100

    Merge Game.Session into Game.Conversation

--- lib/game/conversation.ex
+++ lib/game/conversation.ex
@@ -1,6 +1,6 @@
 defmodule Game.Conversation do
   use GenServer
-  alias Game.Session
+  alias Game.Commands
   require Logger
 
   def init(args) do
@@ -23,12 +23,34 @@ defmodule Game.Conversation do
 
   def handle_info({:tcp, port, text}, state) do
     string = text |> List.to_string() |> String.trim()
-    Session.handle(port, string)
+    perform(port, string)
     {:noreply, state}
   end
 
   def handle_info({:tcp_closed, port}, state) do
     Logger.info("Closed #{inspect(port)}")
     {:stop, :normal, state}
+  end
+
+  defp perform(session, message) do
+    Logger.info("got #{message} from #{inspect(session)}")
+
+    program =
+      if message =~ ~r/^\(.*\)$/ do
+        message
+      else
+        "(#{message})"
+      end
+
+    if message =~ ~r/^\W*$/ do
+      output(session, "no input")
+    else
+      # ensure Commands is loaded for Symbelix.run
+      Commands.load()
+
+      result = Symbelix.run(program, Commands)
+
+      output(session, "#{program} -> #{inspect(result)}")
+    end
   end
 end
