a225c1c70fa5a336643b1403b64f3d14cf9e81f6
Karl Eklund <localpart@gmail.com>
2019-07-28 18:32:48 +0200

    Lua timeout test function

--- lib/game.ex
+++ lib/game.ex
@@ -15,4 +15,23 @@ defmodule Game do
   def hello do
     :world
   end
+
+  def test_luerl_sandbox_timeout do
+    st = :luerl_sandbox.init()
+
+    say = fn [message], state ->
+      Game.World.players()
+      |> Enum.each(&Game.Player.notify(&1, {:saying, "test", message}))
+
+      {[message], state}
+    end
+
+    st = :luerl.set_table([:say], say, st)
+
+    {result, state} =
+      :luerl_sandbox.run("x = 0\nwhile true do\nx = x + 1\nsay(x)\nend", st, 100_000)
+
+    IO.puts("done")
+    {result, state}
+  end
 end
